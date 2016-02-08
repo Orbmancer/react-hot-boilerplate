373a853d412e9ab3c4763db3b9f76dbe996687d9
Damien BRY <damienbry@MacBook-Pro-de-Damien-2.local>
2016-02-08 14:28:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,46 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
+  	var elements = [
+  		{ "name": "Alfred"},
+  		{ "name": "Béatrice"},
+  		{ "name": "Wilhem"},
+  		{ "name": "Zlatan"},
+  	];
+
     return (
-      <h1>Hello, world.</h1>
+    	<div>
+      	<h1>Hello, world.</h1>
+      	<List data={ elements }></List>
+      </div>
     );
   }
+}
+
+export class List extends Component {
+	render(){
+		var elements = this.props.data.map(function(element) {
+      return (
+        <ListElement>
+          {element.name}
+        </ListElement>
+      );
+    });
+
+		return (
+				<ul>
+					{ elements }
+				</ul>
+			);
+	}
+}
+
+export class ListElement extends Component {
+	render(){
+		return (
+				<li>
+					{ this.props.children }
+				</li>
+			);
+	}
 }
